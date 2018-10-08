875c2ec5ee5f0be4bd5c31c0fec9efad208e678c
Frédéric Woelffel <woelffel.f@gmail.com>
2018-10-08 23:39:37 +0200

    feat(api): enable graphql playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      playground: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
