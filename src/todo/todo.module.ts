e1117d2b43f7fc817e39eb72e1008e15433016df
Frédéric Woelffel <woelffel.f@gmail.com>
2018-10-16 13:45:19 +0200

    feat(api): add connection to postgres

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.hasOwnProperty('POSTGRES_HOST') ? process.env.POSTGRES_HOST : '127.0.0.1',
+      port: process.env.hasOwnProperty('POSTGRES_PORT') ? Number(process.env.POSTGRES_PORT) : 5432,
+      username: process.env.hasOwnProperty('POSTGRES_USER') ? process.env.POSTGRES_USER : 'default_user',
+      password: process.env.hasOwnProperty('POSTGRES_PASSWORD') ? process.env.POSTGRES_PASSWORD : 'default_password',
+      database: process.env.hasOwnProperty('POSTGRES_DB') ? process.env.POSTGRES_DB : 'default_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
   ],
 })
