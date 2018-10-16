b3f90ee4723f68469c4ee60a3d00a4cd527c325f
Frédéric Woelffel <woelffel.f@gmail.com>
2018-10-16 13:45:44 +0200

    style(api): lint project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [
-    TodoModule,
-  ],
+  imports: [TodoModule],
   controllers: [],
   providers: [],
 })
